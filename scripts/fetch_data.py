a23a896e6879e1cdf1cb4bf71cbba3cf59a97aa6
Kale Kundert <kale@thekunderts.net>
2014-10-20 12:09:56 -0700

    Make the fetch and push scripts easier to use.

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -6,6 +6,10 @@ simulation results from the cluster to a workstation for analysis.  The given
 directory must be contained within a directory created by 01_setup_design.
 
 Usage: fetch_data.py [options] <directory>
+
+Options:
+    --remote URL, -r URL
+        
 """
 
 import os, subprocess
@@ -15,20 +19,20 @@ from libraries import pipeline
 with scripting.catch_and_print_errors():
     arguments = docopt.docopt(__doc__)
     directory = arguments['<directory>']
-    workspace = pipeline.workspace_from_dir(directory)
+
+    try:
+        workspace = pipeline.workspace_from_dir(directory)
+        remote_url = workspace.rsync_url
+    except pipeline.WorkspaceNotFound:
+        remote_url = arguments['--remote']
+        if remote_url is None:
+            scripting.print_error_and_die("No remote host specified.")
 
     rsync_command = [
-            'rsync',
-            '-avr' if workspace.rsync_recursive_flag else '-av',
-    ]
-    for pattern in workspace.rsync_include_patterns:
-        rsync_command += ['--include', pattern]
-
-    for pattern in workspace.rsync_exclude_patterns:
-        rsync_command += ['--exclude', pattern]
-        
-    rsync_command += [
-            os.path.join(workspace.rsync_url, directory) + '/', directory
+            'rsync', '-avr',
+            '--exclude', 'rosetta', '--exclude', 'rsync_url',
+            '--exclude', 'stdout', '--exclude', 'stderr',
+            os.path.join(remote_url, directory) + '/', directory
     ]
     subprocess.call(rsync_command)
 
--- scripts/push_data.py
+++ scripts/push_data.py
@@ -5,6 +5,9 @@ Copy design files to a remote destination.  A common application is to copy
 input files onto the cluster before starting big jobs.
 
 Usage: push_to_cluster.py <directory>
+
+Options:
+    --remote URL, -r URL
 """
 
 import os, subprocess
@@ -14,19 +17,19 @@ from libraries import pipeline
 with scripting.catch_and_print_errors():
     arguments = docopt.docopt(__doc__)
     directory = arguments['<directory>']
-    workspace = pipeline.workspace_from_dir(directory)
+
+    try:
+        workspace = pipeline.workspace_from_dir(directory)
+        remote_url = workspace.rsync_url
+    except pipeline.WorkspaceNotFound:
+        remote_url = arguments['--remote']
+        if remote_url is None:
+            scripting.print_error_and_die("No remote host specified.")
 
     rsync_command = [
-            'rsync',
-            '-avr' if workspace.rsync_recursive_flag else '-av',
-    ]
-    for pattern in workspace.rsync_include_patterns:
-        rsync_command += ['--include', pattern]
-
-    for pattern in workspace.rsync_exclude_patterns:
-        rsync_command += ['--exclude', pattern]
-        
-    rsync_command += [
-            directory + '/', os.path.join(workspace.rsync_url, directory),
+            'rsync', '-avr',
+            '--exclude', 'rosetta', '--exclude', 'rsync_url',
+            '--exclude', 'stdout', '--exclude', 'stderr',
+            directory + '/', os.path.join(remote_url, directory),
     ]
     subprocess.call(rsync_command)
