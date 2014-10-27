f2d2a78aa45453bcdf0ceaaec5a6c43ee77d5e28
Kale Kundert <kale.kundert@ucsf.edu>
2014-10-27 13:10:40 -0700

    Remove polling from fetch_and_download_models.py

--- scripts/fetch_and_cache_models.py
+++ scripts/fetch_and_cache_models.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python2
 
 """\
-Simultaneously download models from a remote host and cache a number of 
-distance and score metrics for each one.  This script is meant to be run 
-concurrently with big cluster jobs.  As such, it continues running until no 
-new structures have been processed for over an hour and it ignores hang-up 
-signals on its own.
+Download models from a remote host then cache a number of distance and score 
+metrics for each one.  This script is meant to be called periodically during 
+long running jobs, to reduce the amount of time you have to spend waiting to 
+build the cache at the end.
 
 Usage: fetch_and_cache_models.py <directories>... [options]
 
@@ -14,39 +13,20 @@ Options:
     --restraints PATH
 """
 
-import os, signal
-from signal import *
-from time import time, sleep
+from __future__ import division
+
 from tools import docopt, scripting
 from libraries import structures
 from fetch_data import fetch_data
 
 with scripting.catch_and_print_errors():
-    signal(SIGHUP, SIG_IGN)
-
     args = docopt.docopt(__doc__)
     directories = []
-    sleep_time = 5 * 60
-    job_finished_time = 60 * 60
-    last_activity = time()
 
     for directory in args['<directories>']:
         if os.path.isdir(directory): directories.append(directory)
         else: print "Skipping '{}': not a directory.".format(directory)
 
-    while time() - last_activity < job_finished_time:
-        job_reports = {}
-
-        for directory in directories:
-            report = job_reports[directory] = {}
-            fetch_data(directory, args['--remote'])
-            structures.load(directory, args['--restraints'], job_report=report)
-            print
-
-        for directory in job_reports:
-            if job_reports[directory]['new_records'] > 0:
-                last_activity = time()
-                break
-
-        print "Sleeping..."
-        sleep(sleep_time)
+    for directory in directories:
+        fetch_data(directory, args['--remote'])
+        structures.load(directory, args['--restraints'])
