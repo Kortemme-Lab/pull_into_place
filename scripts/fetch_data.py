77b6857a20872e8e0c9462cb9f5e12644fef9f1b
Kale Kundert <kale.kundert@ucsf.edu>
2015-02-19 11:55:40 -0800

    Add an option to include logs when fetching data.

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -9,10 +9,11 @@ Usage: fetch_data.py [options] <directory>
 
 Options:
     --remote URL, -r URL
+    --include-logs
         
 """
 
-def fetch_data(directory, remote_url=None):
+def fetch_data(directory, remote_url=None, include_logs=False):
     import os, subprocess
     from libraries import pipeline
 
@@ -23,7 +24,14 @@ def fetch_data(directory, remote_url=None):
     rsync_command = [
             'rsync', '-avr',
             '--exclude', 'rosetta', '--exclude', 'rsync_url',
-            '--exclude', 'stdout', '--exclude', 'stderr', '--exclude', '*.sc',
+    ]
+    if not include_logs:
+        rsync_command += [
+                '--exclude', 'stdout',
+                '--exclude', 'stderr',
+                '--exclude', '*.sc',
+        ]
+    rsync_command += [
             os.path.join(remote_url, directory) + '/', directory
     ]
     subprocess.call(rsync_command)
@@ -32,7 +40,7 @@ if __name__ == '__main__':
     from tools import docopt, scripting
     with scripting.catch_and_print_errors():
         args = docopt.docopt(__doc__)
-        fetch_data(args['<directory>'], args['--remote'])
+        fetch_data(args['<directory>'], args['--remote'], args['--include-logs'])
 
 
 
