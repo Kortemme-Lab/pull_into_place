4d3ee13c267261233ea7fce6cf25a7e7b0282b5f
ckrivacic <krivacic@berkeley.edu>
2018-03-02 14:36:58 -0800

    Added guide bar to angle restraints

--- pull_into_place/structures.py
+++ pull_into_place/structures.py
@@ -309,7 +309,7 @@ def read_and_calculate(workspace, pdb_paths):
                 meta = ScoreMetadata(
                         name=key,
                         title='Restraint Satisfaction for {0} (Rad)'.format(res),
-                        guide=0.5, lower=0.0, upper='95%', order=3,
+                        guide=0.5, lower=-1, upper='95%', order=3,
                 )
                 record[key] = np.max(residue_specific_restraint_angles[i])
                 metadata[meta.name] = meta
