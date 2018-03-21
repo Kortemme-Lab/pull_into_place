ce06184a4d3abcfcd2cffac04e2b6072a96c6a17
Kale Kundert <kale@thekunderts.net>
2018-03-21 10:37:23 -0700

    Output weblogos in more file formats.
    This is useful for including them in my lab notebook.

--- pull_into_place/commands/web_logo.py
+++ pull_into_place/commands/web_logo.py
@@ -8,11 +8,12 @@ Usage:
     pull_into_place web_logo <directory> [options]
 
 Options:
-    --output PDF, -o PDF
-        The path where the logo should be saved, in the PDF format.  By 
-        default, the logo is not saved and is instead opened using the program 
-        specified in the $PDF environment variable (or evince if no such 
-        variable is defined).
+    --output PATH, -o PATH
+        The path where the logo should be saved.  The desired file format is 
+        deduced from the extension, with '.pdf', '.svg', '.eps', '.png', 
+        '.jpeg', and '.txt' all supported.  By default, the logo is not saved 
+        and is instead opened using the program specified in the $PDF 
+        environment variable (or evince if no such variable is defined).
 
 If you specify a workspace and a round number, the weblogo will be calculated 
 for all the validated designs in that round.  If you specify a directory, the 
@@ -71,7 +72,19 @@ def main():
         logo_file = tempfile.NamedTemporaryFile(
                 'wb', prefix='weblogo_', suffix='.pdf')
 
-    document = weblogo.pdf_formatter(logo_data, logo_format)
+    ext = os.path.splitext(logo_file.name)[-1]
+    formatters = {
+            '.pdf': weblogo.pdf_formatter,
+            '.svg': weblogo.svg_formatter,
+            '.eps': weblogo.eps_formatter,
+            '.png': weblogo.png_formatter,
+            '.jpeg': weblogo.jpeg_formatter,
+            '.txt': weblogo.txt_formatter,
+    }
+    if ext not in formatters:
+        scripting.print_error_and_die("'{0}' is not a supported file format".format(ext))
+
+    document = formatters[ext](logo_data, logo_format)
     logo_file.write(document)
     logo_file.flush()
 
